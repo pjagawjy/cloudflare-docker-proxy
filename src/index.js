26fd11d60d7c3c9591d6b94132acbb2c69883bdc
pjagawjy <pjagawjy@gmail.com>
2025-03-30 18:20:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.zp09.com": dockerHub,
+  "quay.zp09.com": "https://quay.io",
+  "gcr.zp09.com": "https://gcr.io",
+  "k8s-gcr.zp09.com": "https://k8s.gcr.io",
+  "k8s.zp09.com": "https://registry.k8s.io",
+  "ghcr.zp09.com": "https://ghcr.io",
+  "cloudsmith.zp09.com": "https://docker.cloudsmith.io",
+  "ecr.zp09.com": "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker-staging.zp09.com": dockerHub,
 };
 
 function routeByHosts(host) {
